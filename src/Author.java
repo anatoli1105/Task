5de7344c0a8b31c0698f13c5c4606035ac44b4c5
anatolii <anatoliise@mail.ru>
2023-11-07 11:09:48 +0300

    comit

--- src/Author.java
+++ src/Author.java
@@ -1,2 +1,58 @@
 public class Author {
+
+    private  String name;
+    private String surName;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public Author(String name,String surName,int age) {
+        this.name = name;
+        this.surName = surName;
+        this.age = age;
+    }
+    @Override
+    public boolean equals(Object O) {
+        if (this.getClass() != O.getClass()) {
+            return false;
+        }
+        Author author = (Author) O;
+        return name.equals(author.name);
+
+    }
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        String Author = String.format("%s %s \t (%d г.)", name, surName, age);
+        return Author;
+    }
+
 }
--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,65 @@
 public class Book {
+
+
+    private String nameBook;
+    private int agePublication;
+    private Author authorBook;
+
+    public String getNameBook()
+    {
+        return nameBook;
+    }
+
+    public void setNameBook(String nameBook)
+    {
+        this.nameBook = nameBook;
+    }
+
+    public int getAgePublication()
+    {
+        return agePublication;
+    }
+
+    public void setAgePublication(int agePublication)
+    {
+        this.agePublication = agePublication;
+    }
+
+    public Author getAuthorBook()
+    {
+        return authorBook;
+    }
+
+    public void setAuthorBook(Author authorBook)
+    {
+        this.authorBook = authorBook;
+    }
+
+    public Book(String nameBook, int agePublication, Author authorBook) {
+        this.nameBook = nameBook;
+        this.agePublication = agePublication;
+        this.authorBook = authorBook;
+    }
+    @Override
+    public boolean equals(Object O) {
+        if (this.getClass() != O.getClass()) {
+            return false;
+        }
+        Book book = (Book) O;
+        return nameBook.equals(book.nameBook);
+    }
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash();
+    }
+
+
+    @Override
+    public String toString() {
+        String Book = String.format("название книги -%s \n год написания -%d \n автор книги-%s",
+                nameBook, agePublication, authorBook);
+        return Book;
+    }
 }
+
+
